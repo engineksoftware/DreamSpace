4ec40f1607adf6373899601f98fcc0df2e5e5683
Joseph Kessler <tbjsoft@gmail.com>
2017-02-16 19:23:23 -0500

    Added a service for when it restarts and sets the alarm again. Added the user permission for receiving boot complete. Waiting to see if it works this time.

--- app/src/main/java/enginek/dreamspace/StartReceiver.java
+++ app/src/main/java/enginek/dreamspace/StartReceiver.java
@@ -14,24 +14,14 @@ import java.util.Calendar;
  */
 public class StartReceiver extends BroadcastReceiver {
 
+    private static final String QUICKBOOT = "android.intent.action.QUICKBOOT_POWERON";
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        if(intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)){
 
-
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTimeInMillis(System.currentTimeMillis());
-            calendar.set(Calendar.HOUR_OF_DAY, calendar.get(Calendar.HOUR_OF_DAY));
-            calendar.set(Calendar.MINUTE, 0);
-            calendar.set(Calendar.SECOND, 0);
-            calendar.set(Calendar.AM_PM, calendar.get(Calendar.AM_PM));
-            calendar.add(Calendar.DATE,1);
-            Intent i = new Intent("DATE_RECEIVER");
-            PendingIntent pintent = PendingIntent.getBroadcast(context, 0, i, 0);
-            AlarmManager alarm = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-            alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pintent);
-
-
+        if(intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) || intent.getAction().equals(QUICKBOOT)){
+            Intent service = new Intent(context, StartService.class);
+            context.startService(service);
         }
     }
 }
